55a2b4c744c9bb8344485c62acd2d67bea93ce78
Yamini K <yamini@sequoiaat.com>
2026-01-28 17:58:07 +0530

    file upload

--- karate-config.js
+++ karate-config.js
@@ -1,9 +1,12 @@
 function fn() {
   var config = {};
 
-  // Base URL
+  // Base URL for API requests
   config.catApiBaseUrl = 'https://catfact.ninja/breeds';
+  // Base URL for xml file download
   config.baseUrl = 'https://sample-files.com/downloads/data/xml/complex-nested.xml';
+  // Base URL for file upload UI and API testing
+  //config.uploadUiUrl = 'http://localhost:5500/inputs/upload.html';
   
   // Common headers
   config.commonHeaders = {
@@ -12,6 +15,13 @@ function fn() {
 
   // Common thresholds
   config.maxResponseTime = 2000;
+
+  config.driver = {
+    type: 'playwright',
+    browser: 'chromium',
+    headless: false,
+    playwrightExecutable: 'playwright'
+  };
 
   return config;
 }
